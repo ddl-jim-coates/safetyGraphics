e84cd7b6d3e3f4079107e2084e35c3c3532b2d39
Jim Coates <jim.coates@dominodatalab.com>
2024-06-20 18:02:05 +0000

    New new

--- app.sh
+++ app.sh
@@ -1,3 +1,11 @@
-cd ./R
+#!/bin/bash
 
-Rscript safetyGraphicsApp.R
+# Define the port you want to use
+PORT=3838
+
+# Define the host you want to use
+HOST='0.0.0.0'
+
+cd /R
+
+Rscript -e "shiny::runApp('./', port=$PORT, host='$HOST')"
